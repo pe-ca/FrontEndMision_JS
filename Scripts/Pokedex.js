214e345a2043e9ba4d1ebd8af0be69acf5caa2a5
pe-ca <mr.gloser1@gmail.com>
2022-03-20 23:04:03 -0600

    llamada a api y busqueda de informacion

--- Scripts/Pokedex.js
+++ Scripts/Pokedex.js
@@ -2,28 +2,56 @@ const getNombrePokemon = () => document.getElementById("nombrePokemon").value.to
 
 const getApiUrl = (nombre) => `https://pokeapi.co/api/v2/pokemon/${nombre}`;
 
-const setImgPokemon = (url) => document.getElementById("imgPokemon").src = url;
+const setSrc = (Id, Url) => document.getElementById(Id).src = Url;
+
+const setInnerHTML = (Id, Valor) => document.getElementById(Id).innerHTML = Valor;
+
+const getTipos = (element) => element.type.name + " ";
+
+const getEstadisticas = (element) => element.stat.name + ": " + element.base_stat + "<br>";
+
+const getMovimientos = (element) => element.move.name + ", ";
+
+const concatenaTextos = (array, funcionBusqueda) =>{
+    let cadenaConcatenada = "";
+    array.forEach(
+        element => {
+            cadenaConcatenada += funcionBusqueda(element)
+        });
+    return cadenaConcatenada;
+}
+
+const evaluaRespuesta = (res) =>{
+    if(res.status != "200"){
+        console.log(res);
+        setSrc("imgPokemon", "./Img/pokeball.png")
+        setInnerHTML("Nombre", "Nombre")
+        setInnerHTML("Tipo", "")
+        setInnerHTML("Estadisticas", "")
+        setInnerHTML("Movimientos", "")
+    }
+    else{
+        return res.json();
+    }
+}
+
+const asignaValores = (data) =>{
+    if(data){
+        // console.log(data);
+        setSrc("imgPokemon", data.sprites.front_default);
+        setInnerHTML("Nombre", setInnerHTML("Nombre", data.name.toUpperCase()))
+        setInnerHTML("Tipo", concatenaTextos(data.types, getTipos))
+        setInnerHTML("Estadisticas", concatenaTextos(data.stats, getEstadisticas))
+        setInnerHTML("Movimientos", concatenaTextos(data.moves, getMovimientos))
+        console.log(data.stats[0].base_stat)
+        console.log(data.stats[0].stat.name)
+    }
+}
 
 const fetchPokemon = () =>{
     fetch(getApiUrl(getNombrePokemon()))
-        .then(
-            (res) => {
-                if(res.status != "200"){
-                    console.log(res.status);
-                    setImgPokemon("./Img/pokeball.png")
-                }
-                else{
-                    return res.json();
-                }
-            }
-        )
-        .then(
-            (data)=>{
-                if(data){
-                    console.log(data);
-                    setImgPokemon(data.sprites.front_default);
-                }
-            }
-        )
-    console.log(getApiUrl(getNombrePokemon()));
+        .then((res) => evaluaRespuesta(res))
+        .then((data) => asignaValores(data))
 }
+
+//rayquaza
